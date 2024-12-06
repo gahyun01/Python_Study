dbf7c1697e7561f10de62fce611d83a1539ed072
gahyun <135098077+gahyun01@users.noreply.github.com>
2024-12-06 11:02:38 +0900

    Create 2_6_parameters.py

---
+++ 2_6_parameters.py
@@ -0,0 +1,11 @@
+def say_hello(user_name):
+  print("hello", user_name, "how r u?")
+
+
+# user_name은 함수 안에서만 유효함
+# print(user_name) # 오류 발생
+
+say_hello("nico")
+say_hello("lynn")
+say_hello("lewis")
+say_hello("ralph")
