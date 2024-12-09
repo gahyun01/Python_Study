6cf69df02b6387dc9c3e73197af477dd852a556b
gahyun <135098077+gahyun01@users.noreply.github.com>
2024-12-09 11:21:21 +0900

    Create 2_9_hw_calculator.py

---
+++ 2_9_hw_calculator.py
@@ -0,0 +1,26 @@
+def pluse(a=0, b=0):
+  print(a + b)
+
+def minus(a=0, b=0):
+  print(a - b)
+
+def multiplication(a=0, b=0):
+  print(a * b)
+
+def division(a=0, b=1):
+  print(a / b)
+
+def power_of(a=0, b=1):
+  print(a**b)
+
+pluse(11, 2)
+minus(5, 3)
+multiplication(5, 3)
+division(15, 3)
+power_of(4)
+
+pluse()
+minus()
+multiplication()
+division()
+power_of()
