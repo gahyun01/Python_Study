5dba7dcca7c2a0d5718f4ebd77a39fd60a11e6fc
gahyun <135098077+gahyun01@users.noreply.github.com>
2024-12-10 15:42:57 +0900

    Create 3_2_recap.py

---
+++ 3_2_recap.py
@@ -0,0 +1,16 @@
+winner = 50 # elif 3
+winner = 500 # Else
+
+# if winner != 10:
+  # print("Winner is less than 10")
+  # print("If")
+if winner <= 10:
+  print("If")
+elif winner <= 25:
+  print("elif")
+elif winner == 0:
+  print("elif 2")
+elif winner == 50:
+  print("elif 3")
+else:
+  print("Else")
