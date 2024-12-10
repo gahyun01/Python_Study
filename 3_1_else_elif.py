ef465acbce1968807a8be4aa1d804b03265c9d2f
gahyun <135098077+gahyun01@users.noreply.github.com>
2024-12-10 15:32:26 +0900

    Create 3_1_else_elif.py

---
+++ 3_1_else_elif.py
@@ -0,0 +1,19 @@
+""" if와 else """
+# password_correct = True
+password_correct = False
+
+if password_correct:
+  print("Here is your money")
+else:
+  print("Wrong password")
+
+
+""" if, elif, else """
+winner = 10
+
+if winner > 10:
+  print("Winner is greater than 10")
+elif winner < 10:
+  print("Winner is less than 10")
+else:
+  print("Winner is 10")
