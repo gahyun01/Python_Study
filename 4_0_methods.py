1104eabb2cce91f97684ba007cbff3f121da91b5
gahyun <135098077+gahyun01@users.noreply.github.com>
2024-12-19 13:54:09 +0900

    Create 4_0_methods.py

---
+++ 4_0_methods.py
@@ -0,0 +1,9 @@
+# days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+# print(days_of_week)
+
+name = "nico"
+print(name.upper()) # 모두 대문자
+print(name.capitalize()) # 첫 글자 대문자
+print(name.startswith("n")) # 'n'dm로 시작하는지 확인 ( bool )
+print(name.replace("o", "😍")) # 'o'를 😍로 바꾸기
+print(name.endswith("o")) # 'o'로 끝나는지 확인 ( bool )
